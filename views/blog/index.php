716500c9be04fa92d80919005a6613839d0e5006
Aith33 <iamtyler@hotmail.fr>
2022-08-10 16:08:46 +0200

    MVC version 10 aout 2022

---
+++ views/blog/index.php
@@ -0,0 +1,3 @@
+<h1>Homepage</h1>
+
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit obcaecati debitis fugiat fugit reprehenderit laudantium culpa, voluptatem repellat odio porro ratione doloremque. Tempora corporis autem beatae ea maxime repellat velit.</p>
---
+++ views/layout.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Time2Watch</title>
+    <link rel="stylesheet" href="<?= SCRIPT . "CSS" . DIRECTORY_SEPARATOR . "app.css" ?>">
+</head>
+<body>
+    <div class="container">
+    <?= $content ?>
+</body>
+</html>
